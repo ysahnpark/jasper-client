dadf8bff69fe4bb79619f826c7603d7d97847f0f
Young Suk Ahn <ys.ahnpark@gmail.com>
2015-01-27 17:46:12 -0500

    Add GoogleMplayerTTS

--- client/tts.py
+++ client/tts.py
@@ -31,6 +31,8 @@ except ImportError:
 import diagnose
 import jasperpath
 
+import textutils
+
 
 class AbstractTTSEngine(object):
     """
@@ -407,6 +409,9 @@ class PicoTTS(AbstractTTSEngine):
         os.remove(fname)
 
 
+
+
+
 class GoogleTTS(AbstractMp3TTSEngine):
     """
     Uses the Google TTS online translator
@@ -556,6 +561,67 @@ class MaryTTS(AbstractTTSEngine):
             tmpfile = f.name
         self.play(tmpfile)
         os.remove(tmpfile)
+
+
+class GoogleMplayerTTS(AbstractTTSEngine):
+    """
+    Uses the google through mplayer
+    """
+
+    SLUG = "google_mplayer-tts"
+
+    def __init__(self, language="en"):
+        super(self.__class__, self).__init__()
+        self.language = language
+
+    @classmethod
+    def is_available(cls):
+        return (super(cls, cls).is_available() and
+                diagnose.check_network_connection())
+
+    @classmethod
+    def get_config(cls):
+        # FIXME: Replace this as soon as we have a config module
+        config = {}
+        # HMM dir
+        # Try to get hmm_dir from config
+        profile_path = jasperpath.config('profile.yml')
+        if os.path.exists(profile_path):
+            with open(profile_path, 'r') as f:
+                profile = yaml.safe_load(f)
+                if ('google-tts' in profile and
+                   'language' in profile['google-tts']):
+                    config['language'] = profile['google-tts']['language']
+
+        return config
+
+    @property
+    def languages(self):
+        langs = ['af', 'sq', 'ar', 'hy', 'ca', 'zh-CN', 'zh-TW', 'hr', 'cs',
+                 'da', 'nl', 'en', 'eo', 'fi', 'fr', 'de', 'el', 'ht', 'hi',
+                 'hu', 'is', 'id', 'it', 'ja', 'ko', 'la', 'lv', 'mk', 'no',
+                 'pl', 'pt', 'ro', 'ru', 'sr', 'sk', 'es', 'sw', 'sv', 'ta',
+                 'th', 'tr', 'vi', 'cy']
+        return langs
+
+    def say(self, phrase):
+        self._logger.debug("Saying '%s' with '%s'", phrase, self.SLUG)
+        if self.language not in self.languages:
+            raise ValueError("Language '%s' not supported by '%s'",
+                             self.language, self.SLUG)
+
+        text_chunk = textutils.segment_text(phrase, 99);
+
+        for text in text_chunk:
+
+            cmd = ['/usr/bin/mplayer', '-ao', 'alsa:device=hw=1,0', '-really-quiet', '-noconsolecontrols'
+                'http://translate.google.com/translate_tts?tl=' + self.language + '&q="' + text + '"']
+            with tempfile.TemporaryFile() as f:
+            subprocess.call(cmd, stdout=f, stderr=f)
+            f.seek(0)
+            output = f.read()
+            if output:
+                self._logger.debug("Output was: '%s'", output)
 
 
 def get_default_engine_slug():
